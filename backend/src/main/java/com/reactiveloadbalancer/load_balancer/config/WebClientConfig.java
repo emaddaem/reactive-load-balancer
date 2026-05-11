bc66517826d36f7c60dc4ba7fa00c404bbbec032
emad <emaddaem@gmail.com>
2026-05-11 22:53:04 +0200

    feat: add WebClient.Builder bean definition

--- backend/src/main/java/com/reactiveloadbalancer/load_balancer/config/WebClientConfig.java
+++ backend/src/main/java/com/reactiveloadbalancer/load_balancer/config/WebClientConfig.java
@@ -1,6 +1,8 @@
 package com.reactiveloadbalancer.load_balancer.config;
 
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -12,6 +14,12 @@ import java.time.Duration;
 public class WebClientConfig {
 
     private final LoadBalancerProperties properties;
+
+    @Bean
+    @ConditionalOnMissingBean
+    public WebClient.Builder webClientBuilder() {
+        return WebClient.builder();
+    }
 
     @Bean
     public WebClient webClient(WebClient.Builder builder) {
